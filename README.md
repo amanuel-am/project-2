e8bd2c0361e016b31f134d8640b3c4c45e70ebcb
amaniz1st@gmail.com <amaniz1st@gmail.com>
2024-06-13 21:36:35 -0500

    read me added

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # project-2
+
+# books.ipynb
+the project fetches data from two sources
+    1 Web scraping from http://books.toscrape.com
+    2 API scraping from google books https://www.googleapis.com/books/v1/volumes
+
+the web scraping fetches data of 500 books
+        Title           object
+        Price           object
+        Availability    object
+        Rating          object
+        Link            object
+        dtype: object
+
+the API scraping fetches details of the corresponding 500 books (Takes approximately 17 mins, change )
+change the page range to smaller integer to run it faster and it will scrape few books 
+        # Loop through 25 pages
+        for page in range(1, 26):
+
+        Title             object
+        Authors           object
+        Publisher         object
+        Published Date    object
+        Description       object
+        ISBN-13           object
+        dtype: object
+
+books.ipynb gather the data from two different sources and store the data in CSV format
+        1 books_toscrape.csv
+        2 books_detail.csv
+
+# Transform and load.ipynb
+
+read the above two csv data
+clean the data
+fill empty values
+transform the data
+save new cleaned df
+        1 cleaned_books.csv
+        2 cleaned_books_detail.csv
+
+# book_schemata.sql
+
+create sql tables
+relationship between tables
+2nf
+joins
+
+# ERD
+
+
+
--- book_schemata.sql
+++ book_schemata.sql
@@ -2,31 +2,51 @@
 
 CREATE TABLE books(
 	book_id SERIAL PRIMARY KEY,
-	title VARCHAR(100) NOT NULL,
-	price VARCHAR(10) NOT NULL,
-	availability VARCHAR(10) NOT NULL,
-	rating VARCHAR(10) NOT NULL,
+	title VARCHAR(255) NOT NULL,
+	price VARCHAR(20) NOT NULL,
+	availability VARCHAR(20) NOT NULL,
+	rating VARCHAR(20) NOT NULL,
 	link VARCHAR(1000) NOT NULL
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 ----------------------------------------------------------------
 SELECT * FROM books
-
+--drop table books
 
 -- CREATE book_detail TABLE-------------------------------------
 
 CREATE TABLE book_details(
 	book_detail_id SERIAL PRIMARY KEY,
-	title VARCHAR(100) NOT NULL,
-	author VARCHAR(50) NOT NULL,
-	publisher VARCHAR(50) NOT NULL,
-	publish_date Date NOT NULL,
-	description VARCHAR(1000) NOT NULL,
-	isbn-13 INT(100) NOT NULL,
-	FOREIGN KEY (book_detail_id) REFERENCES books(book_id),
+	title VARCHAR(255) NOT NULL,
+	author VARCHAR(255) NOT NULL,
+	publisher VARCHAR(1000) ,
+	publish_date VARCHAR(255) NOT NULL,
+	description VARCHAR(1500) NOT NULL,
+	isbn_13 VARCHAR(50) ,
+	FOREIGN KEY (book_detail_id) REFERENCES books(book_id)
 );
 ----------------------------------------------------------------
 SELECT * FROM book_details
+--drop table book_details
 
+------------------------------------------------------------------
+
+SELECT
+    b.book_id,
+    b.title AS book_title,
+    b.price,
+    b.availability,
+    b.rating,
+    b.link,
+    bd.book_detail_id,
+    bd.author,
+    bd.publisher,
+    bd.publish_date,
+    bd.description,
+    bd.isbn_13
+FROM
+    books b
+INNER JOIN
+    book_details bd
+ON
+    b.book_id = bd.book_detail_id;
